d1b5e883f670b4a3ef095187773cb49f68d732d8
CytechYT <Cytechsim@Gmail.com>
2019-09-10 12:29:38 -0400

    Create not-premium.js

---
+++ modules/functions/POST/not-premium.js
@@ -0,0 +1,12 @@
+module.exports = function(message, type, role, data) {
+	const { bot } = require('../../index');
+	const { discord, config } = bot.tools;
+  const e = new discord.RichEmbed()
+    .setTitle('Priemium Command')
+    .setColor(config.colors.error)
+    .setDescription(`Sorry, but the command you are trying to use is a premium only command. If you wish to get a premium membership please use the \`${data.server.prefix}donate\` to donate and become a premium user.`)
+  return message.channel.send(e).then(msg => {
+    msg.delete(10000)
+  })
+  .catch()
+}
