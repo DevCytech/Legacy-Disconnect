bf3fbdb6a8541f0703c2507d041c8d29fa8e6dee
CytechYT <Cytechsim@Gmail.com>
2019-09-09 22:17:56 -0400

    Create error.js

---
+++ modules/functions/POST/error.js
@@ -0,0 +1,20 @@
+module.exports = function(message, text) {
+	const { discord, config } = require('../../lib/utils');
+	let errorTime = getTime();
+	const e = new discord.RichEmbed()
+		.setTitle('An error has occurred.')
+		.setDescription(text)
+		.setFooter(`An error has occurred at ${errorTime}`)
+		.setColor(config.colors.error);
+	return message.channel.send(e);
+};
+
+function getTime() {
+	let date = new Date();
+
+	if (date.getHours() >= 13) {
+		return date.getHours() - 12 + ':' + date.getMinutes() + ' PM';
+	} else {
+		return date.getHours() + ':' + date.getMinutes() + ' AM';
+	}
+}
