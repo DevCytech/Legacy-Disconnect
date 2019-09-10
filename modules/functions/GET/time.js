9a26fd3f502a59e501ceb9e67912af28f00fd151
CytechYT <Cytechsim@Gmail.com>
2019-09-09 22:19:48 -0400

    Create time.js

---
+++ modules/functions/GET/time.js
@@ -0,0 +1,9 @@
+  module.exports = function getTime() {
+	let date = new date();
+
+	if (date.getHours() >= 13) {
+		return date.getHours() - 12 + ':' + date.getMinutes();
+	} else {
+		return date.getHours() + ':' + date.getMinutes();
+	}
+};
