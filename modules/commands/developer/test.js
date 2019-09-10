7917cef54452cc265e4080baf584d4b632bdc2ed
CytechYT <Cytechsim@Gmail.com>
2019-09-09 22:02:47 -0400

    Update test.js

---
+++ modules/commands/developer/test.js
@@ -0,0 +1,32 @@
+module.exports.run = async (bot, data, msg) => {
+	// Variables
+	
+	// Code
+	
+	// Functions
+	
+}
+
+module.exports.config = {
+	"cmd": {
+		"main":"",
+		"aliases":[]
+	},
+	"info": {
+		"name": "",
+		"ussage": "",
+		"description": ""
+	},
+	"module": {
+		"main": "",
+		"sub": ""
+	},
+	"settings": {
+		"restrictions": "0", // 0 - Everyone, 1 - Admin, 2 - Guild Owner, 3 - Dev Team
+		"premium": false,
+		"permissions": {
+			"bot": ["SEND_MESSAGES"],
+			"user": []
+		}
+	}
+}
