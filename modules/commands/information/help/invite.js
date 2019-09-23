99a7a3da1e966dd4b66ad8dcdf8e3d6c7c44cf56
CytechYT <Cytechsim@Gmail.com>
2019-09-23 14:47:35 -0400

    Update invite.js

--- modules/commands/information/help/invite.js
+++ modules/commands/information/help/invite.js
@@ -1,7 +1,17 @@
 module.exports.run = async (bot, message, args, tools, data) => {
 	// Variables
-	
+	const {discord,config} = tools
 	// Code
+	const e = new discord.RichEmbed()
+		.setColor(config.colors.main)
+	
+	if (message.member.hasPermission('CREATE_INSTANT_INVITE') && bot.member.hasPermission('CREATE_INSTANT_INVITE') {
+	    e.addField('Server Invite', `This is your servers invite link: ${getInvite().link}`)
+	}
+	e.addField('Bot Invite Link', `Invite the bot [here](${config.info.links.botInvite.short}): ${config.info.links.botInvite.short}`)
+	e.addField('Bot Support Server', `Join our support server [here](${config.info.links.supportServer.link}): ${config.info.links.supportServer.link} or use code \`${${config.info.links.supportServer.code}}\``)	
+
+	return message.channel.send(e)
 	// Functions
 	function getInvite() {}
 };
@@ -25,7 +35,7 @@ module.exports.config = {
 		restrictions: 0, // 0 - Everyone, 1 - Admin, 2 - Guild Owner, 3 - Dev Team
 		premium: false,
 		permissions: {
-			bot: ['SEND_MESSAGES'],
+			bot: ['SEND_MESSAGES', 'EMBED_LINKS'],
 			user: []
 		}
 	}
