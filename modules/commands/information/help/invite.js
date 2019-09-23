58ae62f93301b72742c4e449baf32762f34cc5d4
CytechYT <Cytechsim@Gmail.com>
2019-09-23 15:12:04 -0400

    Finished Invite Command

--- modules/commands/information/help/invite.js
+++ modules/commands/information/help/invite.js
@@ -1,19 +1,36 @@
 module.exports.run = async (bot, message, args, tools, data) => {
 	// Variables
-	const {discord,config} = tools
+	const { discord, config } = tools;
 	// Code
-	const e = new discord.RichEmbed()
-		.setColor(config.colors.main)
-	
-	if (message.member.hasPermission('CREATE_INSTANT_INVITE') && bot.member.hasPermission('CREATE_INSTANT_INVITE') {
-	    e.addField('Server Invite', `This is your servers invite link: ${getInvite().link}`)
-	}
-	e.addField('Bot Invite Link', `Invite the bot [here](${config.info.links.botInvite.short}): ${config.info.links.botInvite.short}`)
-	e.addField('Bot Support Server', `Join our support server [here](${config.info.links.supportServer.link}): ${config.info.links.supportServer.link} or use code \`${${config.info.links.supportServer.code}}\``)	
+	const e = new discord.RichEmbed().setColor(config.colors.main);
 
-	return message.channel.send(e)
+	if (
+		message.member.hasPermission('CREATE_INSTANT_INVITE') &&
+		message.guild.me.hasPermission('CREATE_INSTANT_INVITE')
+	) {
+		e.addField(
+			'Server Invite',
+			`This is your servers invite link: ${await getInvite()}`
+		);
+	}
+	e.addField(
+		'Bot Invite Link',
+		`Invite the bot [here](${config.info.links.botInvite.short}): ${config.info.links.botInvite.short}`
+	);
+	e.addField(
+		'Bot Support Server',
+		`Join our support server [here](${config.info.links.supportServer.link}): ${config.info.links.supportServer.link} or use code \`${config.info.links.supportServer.code}\``
+	);
+
+	return message.channel.send(e);
 	// Functions
-	function getInvite() {}
+	async function getInvite() {
+		let invite = await message.channel.createInvite({
+			maxAge: 86400,
+			maxUses: 1
+		});
+		return invite;
+	}
 };
 
 module.exports.config = {
@@ -28,8 +45,8 @@ module.exports.config = {
 		description: 'Get a invite link for your server and a '
 	},
 	module: {
-		main: '',
-		sub: ''
+		main: 'information',
+		sub: 'help'
 	},
 	settings: {
 		restrictions: 0, // 0 - Everyone, 1 - Admin, 2 - Guild Owner, 3 - Dev Team
