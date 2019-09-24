9c306f2713bbbe22823d19260f1fea319c9344a8
Cytech <Cytechsim@Gmail.com>
2019-09-24 18:39:18 -0400

    Started working on the restart command.

---
+++ modules/commands/developer/restart.js
@@ -0,0 +1,39 @@
+module.exports.run = async (bot, message, args, tools, data) => {
+	// Variables
+  const { discord, config, creds } = tools
+	// Code
+  if (message.author.id !== config.botInfo.creator.ID) return;
+	message.channel
+		.send('Restarting...')
+		.then(console.clear())
+		.then(msg => bot.destroy())
+		.then(() => bot.login(creds.token))
+		.then(message.channel.send('Bot Restarted!'))
+    .then(require(../../../../lib/database));
+	// Functions
+};
+
+module.exports.config = {
+	cmd: {
+		main: 'restart',
+		aliases: ['reboot']
+	},
+	info: {
+		name: 'Restart',
+		usage: 'restart',
+		aliases: 'reboot',
+		description: 'Restart the bot.'
+	},
+	module: {
+		main: 'developer',
+		sub: 'tool'
+	},
+	settings: {
+		restrictions: 3, // 0 - Everyone, 1 - Admin, 2 - Guild Owner, 3 - Dev Team
+		premium: false,
+		permissions: {
+			bot: ['SEND_MESSAGES'],
+			user: []
+		}
+	}
+};
