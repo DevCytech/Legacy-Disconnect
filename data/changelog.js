d1373982a1928cf363a3c1f720c1996cb0ddb64a
Cytech <Cytechsim@Gmail.com>
2019-09-25 09:26:49 -0400

    Update changelog.js

--- data/changelog.js
+++ data/changelog.js
@@ -1,5 +1,11 @@
-let changelog = `📌 __**${
-	require('./configs/config.json').settings.version
-}**__ \n\n • New code structure. \n • New economy system \n • New leveling system`;
+let changelog = `📌 __**${require('./configs/config.json').settings.version}**__ 
+\n\n • A Complete Rebuild of the bot \n • Improved Preformance and Data Storage \n • Redesigned Interface \n • New Guild Management Tools \n • New Economy System for all your economy needs \n • New leveling system with some neat features \n • Premium System Added w/ User Profiles
+`;
 
 module.exports = changelog;
+
+/*
+📌 (Version #)
+
+Changes:
+*/
