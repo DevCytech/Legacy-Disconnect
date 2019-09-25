17e4ff4904c57f7c94f792d21869df9e28f456ba
Cytech <Cytechsim@Gmail.com>
2019-09-25 12:14:02 -0400

    Create disconnect.js

---
+++ modules/events/disconnect.js
@@ -0,0 +1,22 @@
+const { bot } = require('../../index');
+
+bot.on('warn', async event => {
+  const { config, discord } = bot.tools
+  
+  const e = new discord.RichEmbed()
+    .setTitle('🎶 Disconnected!')
+    .setColor(config.colors.warning)
+    .setDescription(event)
+    .setFooter(getTime())
+  return bot.channels.get("id", config.consoleAux).send(e);
+});
+
+function getTime() {
+	let date = new Date();
+
+	if (date.getHours() >= 13) {
+		return date.getHours() - 12 + ':' + date.getMinutes() + ' PM';
+	} else {
+		return date.getHours() + ':' + date.getMinutes() + ' AM';
+	}
+}
