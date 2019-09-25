6698e011afd533249ab3b5ae8039901a4d050393
Cytech <Cytechsim@Gmail.com>
2019-09-25 12:19:54 -0400

    Update error.js

--- modules/events/error.js
+++ modules/events/error.js
@@ -1,5 +1,23 @@
 const { bot } = require('../../index');
 
 bot.on('error', async err => {
-	console.log(err);
+	const { config, discord } = bot.tools
+  
+	const e = new discord.RichEmbed()
+		.setTitle('❗Error!')
+		.setColor(config.colors.warning)
+		.setDescription(err)
+		.setFooter(getTime())
+	bot.channels.get("id", config.console).send(e);
+	return console.log(err);
 });
+
+function getTime() {
+	let date = new Date();
+
+	if (date.getHours() >= 13) {
+		return date.getHours() - 12 + ':' + date.getMinutes() + ' PM';
+	} else {
+		return date.getHours() + ':' + date.getMinutes() + ' AM';
+	}
+}
