1e2df00b9c8a9a04d8608ab408709eb38c041050
Cytech <Cytechsim@Gmail.com>
2019-09-25 12:32:01 -0400

    Create guildCreate.js

---
+++ modules/events/guildCreate.js
@@ -0,0 +1,22 @@
+const { bot } = require('../../index');
+
+bot.on('guildCreate', async guild => {
+  const { config, discord } = bot.tools
+  
+  const e = new discord.RichEmbed()
+    .setTitle('New Guild Joined!')
+    .setColor(config.colors.Success)
+    .setDescription(`I jave just joined ${guild.name} with the id of ${guild.id}`)
+    .setFooter(getTime())
+  return bot.channels.get("id", config.console).send(e);
+});
+
+function getTime() {
+	let date = new Date();
+
+	if (date.getHours() >= 13) {
+		return date.getHours() - 12 + ':' + date.getMinutes() + ' PM';
+	} else {
+		return date.getHours() + ':' + date.getMinutes() + ' AM';
+	}
+}
