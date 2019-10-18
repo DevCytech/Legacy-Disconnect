56a8de5f5b0d16ef8027a43e26807b90016ea231
Cytech <Cytechsim@Gmail.com>
2019-10-18 10:00:51 -0400

    Update bad-usage-error.js
    Improving Code Quality and Removing Duplication (adding utility get time instead of manual get time on all.)

--- modules/functions/POST/bad-usage-error.js
+++ modules/functions/POST/bad-usage-error.js
@@ -1,5 +1,5 @@
 module.exports = function(cmd, message, data, text) {
-	const { discord, config } = require('../../../lib/utilities');
+	const { discord, config, getTime } = require('../../../lib/utilities');
 	const { bot } = require('../../../index');
 
 	let command = bot.commands.get(cmd);
@@ -18,17 +18,3 @@ module.exports = function(cmd, message, data, text) {
 	}
 	return message.channel.send(e);
 };
-
-function getTime() {
-	let date = new Date();
-	if (date.getMinutes() >= 9) {
-		minutes = '0' + date.getMinutes();
-	} else {
-		minutes = date.getMinutes();
-	}
-	if (date.getHours() >= 13) {
-		return date.getHours() - 12 + ':' + minutes + ' PM';
-	} else {
-		return date.getHours() + ':' + minutes + ' AM';
-	}
-}
