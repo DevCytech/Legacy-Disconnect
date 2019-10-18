2e5bc957cf53547d2283bd4fd3c8c04ed481e4c1
Cytech <Cytechsim@Gmail.com>
2019-10-18 08:32:01 -0400

    Update guildCreate.js
    Worked on improving code quality adding curly brackets as well as changing the getTime function.

--- modules/events/guildCreate.js
+++ modules/events/guildCreate.js
@@ -1,7 +1,7 @@
 const { bot } = require('../../index');
 
 bot.on('guildCreate', async guild => {
-	const { config, discord } = bot.tools;
+	const { config, discord, getTime } = bot.tools;
 
 	const e = new discord.RichEmbed()
 		.setTitle('New Guild Joined!')
@@ -11,16 +11,8 @@ bot.on('guildCreate', async guild => {
 		)
 		.setFooter(getTime());
 	var channel = bot.channels.get('622619858600984586');
-	if (channel == undefined || channel == null) return;
+	if (channel == undefined || channel == null) {
+		return;
+	}
 	channel.send(e);
 });
-
-function getTime() {
-	let date = new Date();
-
-	if (date.getHours() >= 13) {
-		return date.getHours() - 12 + ':' + date.getMinutes() + ' PM';
-	} else {
-		return date.getHours() + ':' + date.getMinutes() + ' AM';
-	}
-}
