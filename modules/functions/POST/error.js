00c2d0fde2b86de9c095c2d199f2d0883b45869f
Cytech <Cytechsim@Gmail.com>
2019-10-18 10:01:23 -0400

    Update error.js
    Improving Code Quality and Removing Duplication (adding utility get time instead of manual get time on all.)

--- modules/functions/POST/error.js
+++ modules/functions/POST/error.js
@@ -1,24 +1,9 @@
 module.exports = function(message, text) {
-	const { discord, config } = require('../../../lib/utilities');
-	let errorTime = getTime();
+	const { discord, config, getTime } = require('../../../lib/utilities');
 	const e = new discord.RichEmbed()
 		.setTitle('An error has occurred.')
 		.setDescription(text)
-		.setFooter(`An error has occurred at ${errorTime}`)
+		.setFooter(`An error has occurred at ${getTime()}`)
 		.setColor(config.colors.error);
 	return message.channel.send(e);
 };
-
-function getTime() {
-	let date = new Date();
-	if (date.getMinutes() >= 9) {
-		minutes = '0' + date.getMinutes();
-	} else {
-		minutes = date.getMinutes();
-	}
-	if (date.getHours() >= 13) {
-		return date.getHours() - 12 + ':' + minutes + ' PM';
-	} else {
-		return date.getHours() + ':' + minutes + ' AM';
-	}
-}
