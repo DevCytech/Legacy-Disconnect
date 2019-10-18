ec0a764fc18a16547fd97f24fc4d2610741a8ecb
Cytech <Cytechsim@Gmail.com>
2019-10-18 10:05:53 -0400

    Update verifyPermissions.js
    Improving Code Quality and Adding in Variable Definitions

--- modules/functions/methods/verifyPermissions.js
+++ modules/functions/methods/verifyPermissions.js
@@ -1,5 +1,5 @@
 module.exports = function(bot, message, command, tools, data) {
-	let missing = [];
+	let missing = [], val;
 	command.config.settings.permissions.user.forEach(permission => {
 		if (!message.member.hasPermission(permission)) {
 			missing.push(permission);
