1b785a326bc669a28d4bea14c01b6c8e6c42d32f
Cytech <Cytechsim@Gmail.com>
2019-10-18 10:01:50 -0400

    Update bad-usage-error.js
    Improving Code Quality

--- modules/functions/POST/bad-usage-error.js
+++ modules/functions/POST/bad-usage-error.js
@@ -2,14 +2,13 @@ module.exports = function(cmd, message, data, text) {
 	const { discord, config, getTime } = require('../../../lib/utilities');
 	const { bot } = require('../../../index');
 
-	let command = bot.commands.get(cmd);
 	let errorTime = getTime();
 	const e = new discord.RichEmbed()
 		.setTitle('Improper Ussage:')
 		.setDescription(
 			`__Command Usage:__ ${data.guild.main.prefix}${command.config.info.usage}`
 		)
-		.setFooter(`An error has occurred at ${errorTime}`)
+		.setFooter(`An error has occurred at ${getTime()}`)
 		.setColor(config.colors.error);
 	if (text) {
 		e.setDescription(
