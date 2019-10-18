e7951eaafab8b7bccb9b8ec903907ccc2b6cd8d8
Cytech <Cytechsim@Gmail.com>
2019-10-18 10:03:33 -0400

    Delete custom-commands.js
    Unused Module. (CC are being rewritten as this module doesn't work)

--- modules/functions/RUN/custom-commands.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(bot, message, cc) {
-	const { config, discord } = bot.tools;
-
-	const e = new discord.RichEmbed()
-		.setTitle(cmd.name)
-		.setDescription(cmd.response)
-		.setColor(config.colors.regular);
-	return message.channel.send(e);
-};
